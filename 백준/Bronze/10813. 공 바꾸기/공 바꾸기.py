160e2232279aa31a483b8ac6b30979a4f2f95025
Jun <166234904+gitdory@users.noreply.github.com>
2024-04-17 18:51:57 +0900

    Update 공 바꾸기.py
    주석 달았음

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -4,5 +4,6 @@ for r in range(1, N + 1):
     lst.append(r)
 for x in range(M):
     i, j = map(int, input().split())
+    # 리스트 값 교환
     lst[i - 1], lst[j - 1] = lst[j - 1], lst[i - 1]
 print(*lst)
