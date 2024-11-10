d638501552923761076678b94bfae05151312d62
Jun <166234904+nappper@users.noreply.github.com>
2024-11-10 14:08:14 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // A,B,C 입력받기
+    unsigned long A,B,C;
+    cin >> A >> B >> C;
+    // A + B + C 계산하기
+    cout << A + B + C;
+
+    return 0;
+}
