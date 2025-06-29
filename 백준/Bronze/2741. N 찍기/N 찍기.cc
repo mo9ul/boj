cc4391bfae06c7c597a1e932ced3180df32076ff
Jun <166234904+mo9ul@users.noreply.github.com>
2025-06-29 21:35:55 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> N;
+    for(int i = 0; i < N; i++) {
+        cout << i+1 << "\n";
+    }
+
+    return 0;
+}
