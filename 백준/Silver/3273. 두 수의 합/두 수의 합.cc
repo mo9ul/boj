362b6debfd759d5a1957988ff8ec15c3ef8d8433
Jun <166234904+mo9ul@users.noreply.github.com>
2025-07-10 17:54:08 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 7880 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int arr[1000001];
+bool occur[2000001];
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int n, x;
     cin >> n;
-    int arr[n];
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
     cin >> x;
-
-    unordered_map<int, int> m;
-    int cnt = 0;
+    int ans = 0;
     for (int i = 0; i < n; i++) {
-        int target = x - arr[i]; // x - 지금 관찰하는 수
-        if (m[target] > 0) { // 해시맵에 그 타겟값이 있다면 0보다 클테니까
-            cnt += m[target]; // 한 쌍의 한 요소는 다른 쌍의 요소로도 이용될 수 있으니
+        if (x - arr[i] > 0 && occur[x-arr[i]]) {
+            ans++;
         }
-        m[arr[i]]++; // arr[i]라는 값을 키로 추가 하는거지.
+        occur[arr[i]] = true;
     }
-    cout << cnt;
+
+    cout << ans;
 }
