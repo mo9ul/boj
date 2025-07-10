1e9941b89f53650771afa16cdd13c27f49664564
Jun <166234904+mo9ul@users.noreply.github.com>
2025-07-10 16:09:41 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -1,32 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void Int2stk(int num, stack<int>& s) {
-    if(num == 0) {
-        s.push(0);
-    }
-    while(num != 0) {
-        s.push(num % 10);
-        num /= 10;
-    }
-}
-
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int A, B, C;
     cin >> A >> B >> C;
-    int res = A*B*C;
-    stack<int> s;
-    Int2stk(res,s); // 정수 각 자릿수를 스택에 담는 함수
-    int count[10] = {}; // 자릿수 별 카운트를 담을 배열이다. 0~9까지 10개를 받는다.
-
-    while(!s.empty()) { // 스택이 비어있지 않다면, 맨 윗 요소를 확인하고 해당 숫자의 카운트를 올린다. 그러고 pop.
-        count[s.top()]++;
-        s.pop();
+    int mul = A*B*C;
+    vector<int> digits;
+    string s = to_string(mul);
+    for (char c : s) {
+        digits.push_back(c - '0');
+    }
+    int arr[10];
+    fill(arr, arr+10, 0);
+    for (int i : digits) {
+        arr[i]++;
+    }
+    for (int j = 0; j < 10; j++) {
+        cout << arr[j] << '\n';
     }
 
-    for(int i = 0; i < 10; i++) {
-        cout << count[i] << endl;
-    }
-
-    return 0;
 }
