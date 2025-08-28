c32313044ff0c85dea196c1093815561118c965c
Jun <166234904+mo9ul@users.noreply.github.com>
2025-08-28 13:00:54 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    int cnt = 0;
+    while (N >= 0) {
+        if (N % 5 == 0) {
+            cnt += N / 5;
+            cout << cnt;
+            return 0;
+        }
+        N -= 3;
+        cnt++;
+    }
+    cout << -1;
+}
