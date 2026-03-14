6f6a6c7e24d3cfba17dbab229565d1c3b119351d
Jun <166234904+mo9ul@users.noreply.github.com>
2026-03-14 16:00:15 +0900

    [Bronze II] Title: Strfry, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.cc
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -1,31 +1,50 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int N;
-    cin >> N;
-    while (N--) {
-        int a[26] = {};
-        string s1, s2;
-        cin >> s1 >> s2;
-        for (auto c: s1) {
-            a[c-'a']++;
+
+    int n;
+    string a, b;
+    cin >> n;
+
+    // 알파벳 배열을 만든다. (a = 0, b = 1... 이런식)
+    // 문자열을 이터레이팅하면서 알파벳 배열의 값을 증가시킨다.
+    // a면 0번째 값을, b면 1번째 값을 증가시키는 식으로.
+    // 알파벳 배열을 돌면서 홀수가 나오면 Impossible, 안나오면 Possible을 출력한다.
+
+    for (int i = 0; i < n; i++)
+    {
+        int arr_a[26] = {0};
+        int arr_b[26] = {0};
+        bool isValid = true;
+        cin >> a >> b;
+        for (int j = 0; j < a.length(); j++)
+        {
+            arr_a[a[j] - 'a']++;
         }
-        for (auto c: s2) {
-            a[c-'a']--;
+        for (int j = 0; j < b.length(); j++)
+        {
+            arr_b[b[j] - 'a']++;
         }
-        bool isPossible = true;
-        for (auto n: a) {
-            if (n != 0) {
-                isPossible = false;
+
+        for (int j = 0; j < 26; j++)
+        {
+            if (arr_a[j] != arr_b[j])
+            {
+                isValid = false;
+                break;
             }
         }
-        if (isPossible == true) {
+
+        if (isValid == true)
+        {
             cout << "Possible" << '\n';
         }
-        else {
+        else
+        {
             cout << "Impossible" << '\n';
         }
     }
