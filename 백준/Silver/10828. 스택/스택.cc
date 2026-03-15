1ebc4c89df2fd21f1c53f4e36a271c8e700185c5
Jun <166234904+mo9ul@users.noreply.github.com>
2026-03-15 16:16:01 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,68 +1,61 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int arr[1000001];
-int pos = 0;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-void push(int x) {
-    pos++;
-    arr[pos] = x;
-}
-
-void pop() {
-    if (pos == 0) {
-        cout << -1 << '\n';
-    }
-    else {
-        cout << arr[pos] << '\n';
-        pos--;
-    }
-}
-
-void size() {
-    cout << pos << '\n';
-}
-
-void empty() {
-    if (pos == 0) {
-        cout << 1 << '\n';
-    }
-    else {
-        cout << 0 << '\n';
-    }
-}
-
-void top() {
-    if (pos == 0) {
-        cout << -1 << '\n';
-    }
-    else {
-        cout << arr[pos] << '\n';
-    }
-}
-
-int main() {
-    int N;
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        string op;
-        int x;
+    int n, num;
+    string op;
+    vector<int> v;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
         cin >> op;
-        if (op == "push") {
-            cin >> x;
-            push(x);
+        if (op == "push")
+        {
+            cin >> num;
+            v.push_back(num);
         }
-        else if (op == "pop") {
-            pop();
+        else if (op == "pop")
+        {
+            if (v.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << v.back() << '\n';
+                v.pop_back();
+            }
         }
-        else if (op == "size") {
-            size();
+        else if (op == "size")
+        {
+            cout << v.size() << '\n';
         }
-        else if (op == "empty") {
-            empty();
+        else if (op == "empty")
+        {
+            if (v.empty())
+            {
+                cout << 1 << '\n';
+            }
+            else
+            {
+                cout << 0 << '\n';
+            }
         }
-        else if (op == "top") {
-            top();
+        else if (op == "top")
+        {
+            if (v.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << v.back() << '\n';
+            }
         }
     }
 }
