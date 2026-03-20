2a7da19677760238a2dad92dc0652c2bde6a7ba2
Jun <166234904+mo9ul@users.noreply.github.com>
2026-03-20 14:47:02 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,56 +1,73 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int N, M;
-    cin >> N;
-    string op;
-    vector<int> v;
-    int head = 0;
-    int tail = 0;
-    for (int i = 0; i < N; i++) {
+
+    int n;
+    cin >> n;
+
+    queue<int> q;
+    for (int i = 0; i < n; i++)
+    {
+        string op;
         cin >> op;
-        if (op == "push") {
-            cin >> M;
-            v.push_back(M);
-            tail++;
+        if (op == "push")
+        {
+            int i;
+            cin >> i;
+            q.push(i);
         }
-        else if (op == "pop") {
-            if (head == tail) {
+        else if (op == "pop")
+        {
+            if (!q.empty())
+            {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+            else
+            {
                 cout << -1 << '\n';
             }
-            else {
-                cout << v[head] << '\n';
-                head++;
-            }
         }
-        else if (op == "size") {
-            cout << tail - head << '\n';
+        else if (op == "size")
+        {
+            cout << q.size() << '\n';
         }
-        else if (op == "empty") {
-            if (tail - head == 0) {
+        else if (op == "empty")
+        {
+            if (q.empty())
+            {
                 cout << 1 << '\n';
             }
-            else {
+            else
+            {
                 cout << 0 << '\n';
             }
         }
-        else if (op == "front") {
-            if (tail - head != 0) {
-                cout << v[head] << '\n';
-            }
-            else {
+        else if (op == "front")
+        {
+            if (q.empty())
+            {
                 cout << -1 << '\n';
+            }
+            else
+            {
+                cout << q.front() << '\n';
             }
         }
-        else if (op == "back") {
-            if (tail - head != 0) {
-                cout << v[tail-1] << '\n';
-            }
-            else {
+        else
+        {
+            if (q.empty())
+            {
                 cout << -1 << '\n';
+            }
+            else
+            {
+                cout << q.back() << '\n';
             }
         }
     }
