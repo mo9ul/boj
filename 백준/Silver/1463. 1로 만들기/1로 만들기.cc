467c15aa5f483ca3f2d9ac4636e82d068f1023b1
SungJun Hong <166234904+mo9ul@users.noreply.github.com>
2026-04-06 16:19:31 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    int dp[1000001];
+    dp[0] = 0;
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 1;
+
+    cin >> n;
+
+    for (int i = 4; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + 1;
+        if (i % 2 == 0)
+        {
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        }
+        if (i % 3 == 0)
+        {
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+        }
+    }
+
+    cout << dp[n];
+}
