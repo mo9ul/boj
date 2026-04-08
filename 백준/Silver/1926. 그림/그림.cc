d814cb9b9b6474e2c805881ebdc5d3bc1c649ecf
SungJun Hong <166234904+mo9ul@users.noreply.github.com>
2026-04-08 14:21:21 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 3252 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,65 +1,81 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
 #define X first
 #define Y second
 
-int board[502][502];
 bool vis[502][502];
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, -1, 0, 1};
+int board[502][502];
 
-int main() {
+// 순서대로 우,하,좌,상
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, m;
-    cin >> n >> m;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    int height, width;
+    cin >> height >> width;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
             cin >> board[i][j];
         }
     }
 
-    int num = 0;
-    int max = 0;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (board[i][j] != 1 || vis[i][j]) {
+    int paint_cnt = 0;
+    int paint_max = 0;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (board[i][j] == 0 || vis[i][j])
+            {
                 continue;
             }
-            else {
-                num++;
+
+            else
+            {
+                paint_cnt++;
+
                 queue<pair<int, int>> q;
                 vis[i][j] = 1;
-                q.push({i,j});
+                q.push({i, j});
                 int area = 0;
-                while (!q.empty()) {
+                while (!q.empty())
+                {
+                    area++;
                     pair<int, int> cur = q.front();
                     q.pop();
-                    area++;
-                    for (int dis = 0; dis < 4; dis++) {
-                        int nx = cur.X + dx[dis];
-                        int ny = cur.Y + dy[dis];
-                        if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                    for (int dir = 0; dir < 4; dir++)
+                    {
+                        int nx = cur.X + dx[dir];
+                        int ny = cur.Y + dy[dir];
+                        if (nx < 0 || nx >= height || ny < 0 || ny >= width)
+                        {
                             continue;
                         }
-                        if (board[nx][ny] != 1 || vis[nx][ny]) {
+                        if (board[nx][ny] == 0 || vis[nx][ny])
+                        {
                             continue;
                         }
-                        else {
+                        else
+                        {
                             vis[nx][ny] = 1;
                             q.push({nx, ny});
                         }
                     }
-                }
-                if (max <= area) {
-                    max = area;
+                    paint_max = max(paint_max, area);
                 }
             }
         }
     }
-    cout << num << '\n' << max;
+
+    cout << paint_cnt << '\n';
+    cout << paint_max;
 }
